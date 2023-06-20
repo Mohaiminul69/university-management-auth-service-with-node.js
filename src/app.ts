819949c49172c36d065c3dcf1110e040716be27a
mohaiminul69 <mohaiminul69@gmail.com>
2023-06-20 22:42:56 +0600

    service created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userServices from './app/modules/users/user.services'
 const app: Application = express()
 
 app.use(cors())
@@ -7,7 +8,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //! Testing
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+  await userServices.createUser({
+    id: '999',
+    password: '1234',
+    role: 'student',
+  })
   res.send('Hello World!')
 })
 
